acf7e06227effa22da609bebfbf59d669a24f5eb
shafibs20211-crypto <shafibs20211@gmail.com>
2026-09-09 02:29:21 -0700

    props
    props

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Props from "./Components/Props";
+function App() {
+
+  return (
+    <>
+     <h1>Hello </h1>
+     <Props name={"in React js"} info={{name:"Shafiullah", Mobile:"0343555" ,salery:"50000"}}/>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Props.jsx
@@ -0,0 +1,10 @@
+function Props(props) {
+    return(
+        <>
+    <h1>Function {props.name}</h1>
+    <h2> Name:{props.info.name}  Mobile no:{props.info.Mobile} Salery:{props.info.salery}</h2>
+        </>
+    )
+    
+}
+export default Props;
